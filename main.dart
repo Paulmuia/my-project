382f8ae7a6121e16fd69e8db97c241856524078e
Rotse4 <92379862+Rotse4@users.noreply.github.com>
2023-01-26 14:42:37 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,152 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/pages/home/main_home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('The Rental App'),
-          centerTitle: true,
-          backgroundColor: Colors.green,
-
-        ),
-
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Positioned(
-              left: 5.0,
-              top: 5.0,
-              child: Image.asset('assets/t.jpeg'),
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-
-                ElevatedButton(
-                  onPressed: () {},
-                  child:
-                  Text('LandLord'),
-
-                ),
-
-                ElevatedButton(
-                  onPressed: () {},
-
-                  child: Text('Tenant'),
-                ),
-              ],
-            ),
-
-
-            Form(child: Column(
-              children: [
-                SizedBox(height: 20.0,),
-                TextFormField(
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    hintText: 'Enter username',
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(70)
-                    ),
-                  ),
-                  onChanged: (String value) {
-                  },
-                  validator: (value) {
-                    return value!.isEmpty ? 'please enter username' : null;
-                  }
-
-                ),
-                SizedBox(height: 20,),
-
-                TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      hintText: 'Enter email address',
-                      prefixIcon: Icon(Icons.email),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50.0)
-                      ),
-                    ),
-                    onChanged: (String value) {
-                    },
-                    validator: (value) {
-                      return value!.isEmpty ? 'please enter email' : null;
-                    }
-
-                ),
-                  SizedBox(height: 20,),
-                TextFormField(
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: InputDecoration(
-                      hintText: 'Enter password',
-                      prefixIcon: Icon(Icons.lock),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50.0)
-                      ),
-                    ),
-                    onChanged: (String value) {
-                    },
-                    validator: (value) {
-                      return value!.isEmpty ? 'please enter password' : null;
-                    }
-
-                ),
-
-
-                SizedBox(height: 20,),
-                TextFormField(
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: InputDecoration(
-                      hintText: 'confirm password',
-                      prefixIcon: Icon(Icons.lock),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50.0)
-                      ),
-                    ),
-                    onChanged: (String value) {
-                    },
-                    validator: (value) {
-                      return value!.isEmpty ? 'please enter password' : null;
-                    }
-
-                ),
-              ],
-            ),
-            ),
-           SizedBox(height: 20.0,),
-           Row(
-             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-             children: [
-
-               ElevatedButton(
-                 onPressed: () {},
-                 child:
-                 Text('login'),
-
-               ),
-
-               ElevatedButton(
-                 onPressed: () {},
-
-                 child: Text('sign up'),
-               ),
-             ],
-           ),
-          ],
-
-        ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MainFoodPage(),
     );
   }
 }
